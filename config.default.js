9c4e5645c84fb14bec0530979526d60df931c8d5
Yajun Fu <fuyajun1983cn@163.com>
2018-08-30 11:09:01 +0800

    2018年08月30日11时09分01秒

--- config.default.js
+++ config.default.js
@@ -1,8 +1,10 @@
 
 'use strict';
 
-// Exports
-module.exports = {
+// Modules
+var path = require('path');
+
+var config = {
 
   // Your site title (format: page_title - site_title)
   site_title: '个人知识库',
@@ -14,7 +16,7 @@ module.exports = {
   support_email: 'fuyajun1983cn@163.com',
 
   // Footer Text / Copyright
-  copyright: 'Copyright &copy; '+ new Date().getFullYear() +' - <a href="http://raneto.com">Powered by Raneto</a>',
+  copyright: 'Copyright &copy; ' + new Date().getFullYear() + ' - <a href="http://raneto.com">Powered by Raneto</a>',
 
   // Excerpt length (used in search)
   excerpt_length: 400,
@@ -27,34 +29,94 @@ module.exports = {
   // If true category folders need to contain a "sort" file with an integer value
   category_sort: true,
 
+  // Controls behavior of home page if meta ShowOnHome is not present. If set to true
+  // all categories or files that do not specify ShowOnHome meta property will be shown
+  show_on_home_default: true,
+
   // Which Theme to Use?
-  theme_dir  : __dirname + '/node_modules/raneto/themes/',
+  theme_dir  : path.join(__dirname, 'node_modules/raneto', 'themes'),
   theme_name : 'default',
 
   // Specify the path of your content folder where all your '.md' files are located
   // Fix: Needs trailing slash for now!
   // Fix: Cannot be an absolute path
-  content_dir : 'content/',
+  content_dir : path.join(__dirname, 'content'),
 
   // Where is the public directory or document root?
-  public_dir  : __dirname + '/node_modules/raneto/public/',
+  public_dir  : path.join(__dirname, 'node_modules/raneto', 'themes', 'default', 'public'),
 
   // The base URL of your images folder,
   // Relative to config.public_dir
   // (can use %image_url% in Markdown files)
-  image_url: '../images',
+  image_url: '/images',
 
   // Add your analytics tracking code (including script tags)
   analytics: '',
 
   // Set to true to enable the web editor
-  allow_editing : true,
+  allow_editing : false,
 
   // Set to true to enable HTTP Basic Authentication
   authentication : false,
-  credentials    : {
-    username : 'admin',
-    password : 'password'
-  }
+
+  // If editing is enabled, set this to true to only authenticate for editing, not for viewing
+  authentication_for_edit: false,
+
+  // If authentication is enabled, set this to true to enable authentication for reading too
+  authentication_for_read: false,
+
+  // Google OAuth
+  googleoauth: false,
+  oauth2 : {
+    client_id: 'GOOGLE_CLIENT_ID',
+    client_secret: 'GOOGLE_CLIENT_SECRET',
+    callback: 'http://localhost:3000/auth/google/callback',
+    hostedDomain: 'google.com'
+  },
+  secret: 'someCoolSecretRightHere',
+
+  credentials    : [
+    {
+      username : 'admin',
+      password : 'password'
+    },
+    {
+      username : 'admin2',
+      password : 'password'
+    }
+  ],
+
+  locale: 'en',
+
+  // Support search with extra languages
+  searchExtraLanguages: ['ru'],
+
+  // Sets the format for datetime's
+  datetime_format: 'Do MMM YYYY',
+
+  // Set to true to render suitable layout for RTL languages
+  rtl_layout: false,
+
+  // Edit Home Page title, description, etc.
+  home_meta : {
+     title       : '个人知识库',
+     description : '个人学习和工作中搜集的小知识点'
+  },
+
+  // variables: [
+  //   {
+  //     name: 'test_variable',
+  //     content: 'test variable'
+  //   },
+  //   {
+  //     name: 'test_variable_2',
+  //     content: 'test variable 2'
+  //   }
+  // ]
+
+  table_of_contents: false
 
 };
+
+// Exports
+module.exports = config;
